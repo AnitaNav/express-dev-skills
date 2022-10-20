99779d0fdbb6d1b1bc4d9b1cadfa64ca2efdf8ae
AnitaNav <anitakm215@gmail.com>
2022-10-19 22:37:21 -0700

    Part2 lab

--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,27 @@
 const skills = [
-    {id: 125223, skill: 'HTML'},
-    {id: 127904, skill: 'CSS'},
-    {id: 139608, skill: 'JavaScript'}
+    {id: 125223, skill: 'HTML', completed: 'true'},
+    {id: 127904, skill: 'CSS', completed: 'true'},
+    {id: 139608, skill: 'JavaScript', completed: 'false'}
   ];
   
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
   };
+
+  function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
+
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.completed = false;
+    skills.push(skill);
+  }
 
   function getOne(id) {
     id = parseInt(id);
